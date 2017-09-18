5ba41aa6483a0be3b7aaa475e08bafb1f6d841c9
Arvind Sundararajan <arvinds@berkeley.edu>
2017-09-18 15:27:01 -0700

    declared and implemented new openmote debugpins functions: exp (PA3) and pkt (PB5)

--- bsp/boards/debugpins.h
+++ bsp/boards/debugpins.h
@@ -46,6 +46,14 @@ void debugpins_radio_toggle(void);
 void debugpins_radio_clr(void);
 void debugpins_radio_set(void);
 
+void debugpins_exp_toggle(void);
+void debugpins_exp_clr(void);
+void debugpins_exp_set(void);
+
+void debugpins_pkt_toggle(void);
+void debugpins_pkt_clr(void);
+void debugpins_pkt_set(void);
+
 #ifdef OPENSIM
 void debugpins_ka_clr(void);
 void debugpins_ka_set(void);
