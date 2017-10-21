061649be4829d4eb48dc40393ff0f1cca19b5767
Alex Yang <yangalex@berkeley.edu>
2017-10-21 05:25:44 -0700

    added class AnalogDiscoveryUtils

--- openapps/llatency/test_harness/acquisition.py
+++ openapps/llatency/test_harness/acquisition.py
@@ -30,15 +30,21 @@ class Network:
         self.packets_remaining = packets_remaining
 
 
+class AnalogDiscoveryUtils:
+    def __init__(self):
+        self.interface_handler = None
+        self.internal_clock_freq = None
+
 
 def initialize_networks(num_networks):
     """ Initializes all networks """
 
     # keep asking for networks
-    for _ in range(num_networks):
-        output_channels = input("Sending mote channels: ")
-        input_channels = input("Receiving mote channels: ")
-        num_packets_to_send = input ("Number of packets to send: ")
+    for number in range(num_networks):
+        print("Network {}:".format(number))
+        output_channels = input("   Sending mote channels: ")
+        input_channels = input("    Receiving mote channels: ")
+        num_packets_to_send = input ("    Number of packets to send: ")
 
         # create new network 
         network = Network(output_channels, input_channels, num_packets_to_send)
@@ -49,14 +55,28 @@ def initialize_networks(num_networks):
 
         # add to (input channel)->(network) dictionary
         for channel in input_channels:
-            input_channels_to_network(channel] = network
+            input_channels_to_network[channel] = network
 
+def startup():
+    """ Sets the global dwf variable to the appropriate value. Runs on program startup.
+    """
+    if sys.platform.startswith("win"):
+        dwf = cdll.dwf
+    elif sys.platform.startswith("darwin"):
+        dwf = cdll.LoadLibrary("/Library/Frameworks/dwf.framework/dwf")
+    else:
+        dwf = cdll.LoadLibrary("libdwf.so")
 
-
+    #print DWF version
+    version = create_string_buffer(16)
+    dwf.FDwfGetVersion(version)
+    print "DWF Version: "+version.value 
 
 if __name__ == "__main__":
     
     num_networks = input("Number of networks: ")
     initialize_networks(num_networks)
+    startup()
+
 
 
